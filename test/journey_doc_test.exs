e3d73d90f171cb31c19a066bbc711b269df6dd9b
Mark Markaryan <markmark206@gmail.com>
2025-05-14 10:25:13 -0700

    iterating on doctests for Journey and Journey.Node (#29)

---
+++ test/journey_doc_test.exs
@@ -0,0 +1,4 @@
+defmodule JourneyDocTest do
+  use ExUnit.Case, async: true
+  doctest Journey
+end
---
+++ test/journey_node_doc_test.exs
@@ -0,0 +1,4 @@
+defmodule JourneyNodeDocTest do
+  use ExUnit.Case, async: true
+  doctest Journey.Node
+end
