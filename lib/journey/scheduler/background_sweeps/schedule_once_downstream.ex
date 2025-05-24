3f17e57d51cb8dd945afd1b84d372beb956f9065
Mark Markaryan <markmark206@gmail.com>
2025-05-23 17:07:23 -0700

    updating the sweep schedule_once query

--- lib/journey/scheduler/background_sweeps/schedule_once_downstream.ex
+++ lib/journey/scheduler/background_sweeps/schedule_once_downstream.ex
@@ -11,6 +11,30 @@ defmodule Journey.Scheduler.BackgroundSweeps.ScheduleOnceDownstream do
   # TODO: the window value should be a property of the computation, settable by the application.
   @rolling_window_seconds 60 * 60
 
+  defp q_execution_ids_to_advance(execution_id) do
+    now = System.system_time(:second)
+    cutoff_time = now - @rolling_window_seconds
+
+    from(e in q_executions(execution_id),
+      join: c in assoc(e, :computations),
+      join: v in Value,
+      on:
+        v.execution_id == e.id and
+          v.node_name == c.node_name and
+          v.node_type == c.computation_type,
+      where:
+        c.computation_type == :schedule_once and
+          c.state == :success and
+          not is_nil(v.set_time) and
+          (v.node_value <= ^now or
+             fragment("?::bigint", v.node_value) <= ^now) and
+          v.set_time >= ^cutoff_time,
+      # TODO: consider only including executions that have un-computed computations.
+      distinct: true,
+      select: e.id
+    )
+  end
+
   @doc false
   def sweep(execution_id) when is_nil(execution_id) or is_binary(execution_id) do
     # Find and compute all un-computed computations that are downstream of computed schedule_once computations, that are due within the scheduled time window
@@ -18,27 +42,8 @@ defmodule Journey.Scheduler.BackgroundSweeps.ScheduleOnceDownstream do
     prefix = "[#{mf()}] [#{inspect(self())}]"
     Logger.debug("#{prefix}: starting #{execution_id}")
 
-    now = System.system_time(:second)
-    cutoff_time = now - @rolling_window_seconds
-
     kicked_count =
-      from(e in q_executions(execution_id),
-        join: c in assoc(e, :computations),
-        join: v in Value,
-        on:
-          v.execution_id == e.id and
-            v.node_name == c.node_name,
-        where:
-          c.computation_type == :schedule_once and
-            c.state == :success and
-            not is_nil(v.set_time) and
-            (v.node_value <= ^now or
-               fragment("?::bigint", v.node_value) <= ^now) and
-            v.set_time >= ^cutoff_time,
-        # TODO: consider only including executions that have un-computed computations.
-        distinct: true,
-        select: e.id
-      )
+      q_execution_ids_to_advance(execution_id)
       |> Journey.Repo.all()
       |> Enum.map(fn swept_execution_id ->
         swept_execution_id
