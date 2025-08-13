7b3a6cf56043bd394567c10229d3fff27cba0bbe
Mark Markaryan <markmark206@gmail.com>
2025-08-13 11:03:49 -0700

    version to 0.10.19, tidy journey moduledoc (#111)

--- lib/journey/examples/credit_card_application.ex
+++ lib/journey/examples/credit_card_application.ex
@@ -19,17 +19,17 @@ defmodule Journey.Examples.CreditCardApplication do
   iex> execution = execution |> Journey.set_value(:email_address, "mario@example.com")
   iex>
   iex> # This kicks off the pre-approval process, which eventually completes.
-  iex> execution |> Journey.get_value(:preapproval_process_completed, wait_any: 30_000)
+  iex> execution |> Journey.get_value(:preapproval_process_completed, wait_any: true)
   {:ok, true}
   iex> # We haven't heard from the customer, so we'll send a reminder in a few days (seconds;).
-  iex> execution |> Journey.get_value(:send_preapproval_reminder, wait_any: 30_000)
+  iex> execution |> Journey.get_value(:send_preapproval_reminder, wait_any: true)
   {:ok, true}
   iex>
   iex> # Reminded, the customer requests an actual credit card.
   iex> _execution = execution |> Journey.set_value(:credit_card_requested, true)
   iex> # ... which triggers issuing the card.
   iex>
-  iex> execution |> Journey.get_value(:initiate_credit_card_issuance, wait_any: 30_000)
+  iex> execution |> Journey.get_value(:initiate_credit_card_issuance, wait_any: true)
   {:ok, true}
   iex> execution |> Journey.values() |> redact([:schedule_request_credit_card_reminder, :execution_id, :last_updated_at])
   %{
@@ -53,9 +53,9 @@ defmodule Journey.Examples.CreditCardApplication do
   iex> # Eventually, the fulfillment department marks the credit card as mailed.
   iex> # Which triggers an email notifying the customer that the card has been mailed.
   iex> execution = execution |> Journey.set_value(:credit_card_mailed, true)
-  iex> execution |> Journey.get_value(:credit_card_mailed_notification, wait_any: 30_000)
+  iex> execution |> Journey.get_value(:credit_card_mailed_notification, wait_any: true)
   {:ok, true}
-  iex> {:ok, _} = execution |> Journey.get_value(:archive, wait_any: 30_000)
+  iex> {:ok, _} = execution |> Journey.get_value(:archive, wait_any: true)
   iex> # This is only needed in tests.
   iex> Journey.Scheduler.Background.Periodic.stop_background_sweeps_in_test(background_sweeps_task)
 
@@ -187,7 +187,7 @@ defmodule Journey.Examples.CreditCardApplication do
     end
   end
 
-  def long_time_since_last_update?(%{node_value: last_updated_at, execution_id: _execution_id}) do
+  defp long_time_since_last_update?(%{node_value: last_updated_at, execution_id: _execution_id}) do
     System.system_time(:second) - last_updated_at > 60 * 60 * 24
   end
 
