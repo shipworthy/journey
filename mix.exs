b510c9abd56874446763607ef4e7539bc731972f
Mark Markaryan <markmark206@gmail.com>
2026-04-21 10:56:54 -0700

    adding a livebook illustrating journey's retries

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule Journey.MixProject do
           "lib/examples/tick_once.livemd",
           "lib/examples/tick_recurring.livemd",
           "lib/examples/archive.livemd",
+          "lib/examples/compute_retries.livemd",
           "BASIC_CONCEPTS.md",
           "MODULES_AND_FUNCTIONS.md",
           "LICENSE.md",
