8f39370d17d1b2e3593df4291e83ec0986830013
Mark Markaryan <markmark206@gmail.com>
2026-05-11 18:46:29 -0700

    using a stable order when fetching values and computations in an execution (#318)

--- lib/journey/persistence/schema/execution.ex
+++ lib/journey/persistence/schema/execution.ex
@@ -11,10 +11,10 @@ defmodule Journey.Persistence.Schema.Execution do
     field(:graph_version, :string)
     field(:graph_hash, :string)
     field(:archived_at, :integer, default: nil)
-    has_many(:values, Journey.Persistence.Schema.Execution.Value, preload_order: [desc: :ex_revision])
+    has_many(:values, Journey.Persistence.Schema.Execution.Value, preload_order: [desc: :ex_revision, asc: :node_name])
 
     has_many(:computations, Journey.Persistence.Schema.Execution.Computation,
-      preload_order: [desc: :ex_revision_at_completion]
+      preload_order: [desc: :ex_revision_at_completion, asc: :node_name]
     )
 
     field(:revision, :integer, default: 0)
